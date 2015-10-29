41e94f8686f525a9cdf3f4324c7214d9433c323f
profeseba <storresweb@gmail.com>
2015-10-29 18:48:16 -0300

    Super Comit

---
+++ AgentLibrary/AgentLibrary/Agent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AgentLibrary
+{
+    public abstract class Agent 
+    {
+        public abstract Object sensor();
+
+        private void Update()
+        {
+            while (true)
+            {
+                actuador(comportamiento(sensor()));
+            }
+        }
+
+        public abstract String comportamiento(Object obj);
+
+        public abstract void actuador(String accion);
+
+        public void start()
+        {
+            Thread cal = new Thread(new ThreadStart(Update));
+            cal.Start();
+        }
+    }
+}
